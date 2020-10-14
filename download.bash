7fa5d5f1bf5a8902fc5c2e513a875f1b7424b4c9
Simon Niklaus <simon.niklaus@outlook.com>
2020-10-14 12:21:51 -0700

    no message

--- download.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-unflow/network-css.pytorch
-wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-unflow/network-css-synthia.pytorch
